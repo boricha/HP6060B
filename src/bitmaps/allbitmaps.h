01e0e587ed221640f1be8e39d653ca89ae598e30
SS Kim <kim.boricha@gmail.com>
2020-02-22 21:05:40 +0900

    Delete allbitmaps.h

--- src/bitmaps/allbitmaps.h
+++
@@ -1,6 +0,0 @@
-/* allBitmaps.h bitmap header for GLCD library */
-/* This file is created automatically by the glcdMakeBitmap utility */
-/* Any edits to this file will be lost when glcdMakeBitmap is next run */
-
-// WG20232A
-#include "hp52x32.h"
